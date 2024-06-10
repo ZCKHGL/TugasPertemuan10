56b0d06254fabdd2355415a3c5ed9c536bdf7f61
A-12 <A-12@A-12>
2024-06-10 10:57:44 +0700

    pertemuan10

---
+++ TgsPertemuan10/src/main/java/TgsPertemuan10/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TgsPertemuan10;
+
+import java.util.Random;
+
+
+/**
+ *
+ * @author A-12
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+
+    }
+}
